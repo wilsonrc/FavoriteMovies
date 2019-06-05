d7a3bea6cfdcc2eab55ec85f9258f8f4e572b3e3
wilsonrc <wilsonrc16@gmail.com>
2019-06-04 22:49:01 -0400

    add: Discover Movies retrofit endpoint. add: Movies Data Source interface.

---
+++ FavoriteMovies/app/src/main/java/com/wilsonrc/favoritemovies/data/source/MoviesDataSource.kt
@@ -0,0 +1,18 @@
+package com.wilsonrc.favoritemovies.data.source
+
+import com.wilsonrc.favoritemovies.data.models.Movie
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.Single
+
+interface MoviesDataSource {
+
+    fun getMovies() : Observable<List<Movie>>
+
+    fun getFavMovies() : Single<List<Movie>>
+
+    fun saveFavMovies(movie: Movie) : Completable
+
+    fun deleteFavMovies(id: Int) : Completable
+
+}
---
+++ FavoriteMovies/app/src/main/java/com/wilsonrc/favoritemovies/data/source/remote/MoviesService.kt
@@ -0,0 +1,13 @@
+package com.wilsonrc.favoritemovies.data.source.remote
+
+import com.wilsonrc.favoritemovies.data.models.DiscoverResponse
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MoviesService {
+
+    @GET("/discover/movie")
+    fun getAllMovies(@Query("sort") sortBy : String , @Query("year") year : String) : Observable<DiscoverResponse>
+
+}
